e2904ad5468220eee1ead0d743567a2b9deb4561
orlandohuang <orlando.huang8@pm.me>
2022-03-26 13:41:41 +0700

    phew

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, time
 
 args = sys.argv[1:]
 
@@ -6,21 +6,38 @@ def register():
     print("Welcome New User, let's create a new account for PySubOS. ")
     username = input("Username for New User: ")
     password = input("Password for New User: ")
-    print("Thank you for the information, setting up new user. ")
     if os.path.exists("users"):
         os.chdir("users")
     else:
         os.mkdir("users")
         os.chdir("users")
-    os.mkdir(username)
-    os.chdir(username)
+    try:
+        os.mkdir(username)
+        os.chdir(username)
+        print("Thank you for the information, setting up new user. ")
+    except FileExistsError:
+        print("User Exists. ")
+        time.sleep(1)
+        os.system("clear")
+        register()
     with open("password.pwd", "w") as pwd:
         pwd.write(password)
-    
+    os.chdir("../../")
+    time.sleep(2)
+    os.system("clear")
+    login()
+
+def login():
+    if not os.path.exists("users") or not any(os.scandir("users")):
+        register()
+    else:
+        for user in next(os.walk('users'))[1]:
+            print(f"[{next(os.walk('users'))[1].index(user)+1}] {user}")
+        input("\n[?]: ")
 
 if args[0]=="register":
     register()
 elif args[0]=="login":
-    print("login")
+    login()
 else:
     print("error")
